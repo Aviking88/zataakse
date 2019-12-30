27fde80ff2fc8684f207d0ef5574fc628a6c22c2
Avinash Maurya <avinash.gic@gmail.com>
2019-12-30 18:57:00 +0530

    #Added location popups

--- src/app/shared/shared-components/shared-components.module.ts
+++ src/app/shared/shared-components/shared-components.module.ts
@@ -4,16 +4,19 @@ import { SplashScreenComponent } from './splash-screen/splash-screen.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { MaterialModule } from '../material-module/material.module';
 import { TranslateModule } from '@ngx-translate/core';
+import { LocationDialogComponent } from './location-dialog/location-dialog.component';
+import { LocationPopupComponent } from './location-popup/location-popup.component';
 
 
 
 @NgModule({
-  declarations: [SplashScreenComponent, LandingPageComponent],
+  declarations: [SplashScreenComponent, LandingPageComponent, LocationPopupComponent, LocationDialogComponent],
   imports: [
     CommonModule,
     MaterialModule,
     TranslateModule.forChild()
   ],
+  entryComponents: [LocationPopupComponent, LocationDialogComponent],
   exports: [SplashScreenComponent, LandingPageComponent]
 })
 export class SharedComponentsModule { }
